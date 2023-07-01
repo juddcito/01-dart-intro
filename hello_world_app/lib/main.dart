61cdd2c19e27f7a2a54de9b51491ba50767c65c9
Juan Sapien <juansapienflores.is@gmail.com>
2023-07-01 11:56:28 -0700

    61. VoidCallback

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-      body: Center(child: Text('Hola Mundo')),
-    ));
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CounterFunctionsScreen()
+      );
   }
 }
