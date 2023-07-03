9444c79d6741b4fcacd9a97d724df6822776c051
Juan Sapién <juansapienflores@gmail.com>
2023-07-03 10:00:02 -0700

    Ejemplo

--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -24,7 +24,7 @@ class MessageFieldBox extends StatelessWidget {
         onPressed: () {
           // Tomar el valor de una caja de texto
           final textValue = textController.value.text;
-          print('Buttons: $textValue');
+          print('Button: $textValue');
           // Limpiar la caja de texto
           textController.clear();
         },
